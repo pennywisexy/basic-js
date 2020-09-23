50a541834d53ceec0472ee3b101506773990cc62
Alexander Snitko <pennywisexy@gmail.com>
2020-09-23 17:56:33 +0300

    feat: made a function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if( typeof(members) === 'object' && Array.isArray(members)) {
+    members = members
+                   .filter(a => typeof(a) === 'string')
+                   .map(function(b){ b = b.trim().toUpperCase();return b[0]})
+                   .sort()
+                   .join('')
+                   
+  return members;
+  }else return false;
 };
+
