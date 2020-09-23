0ed96896191b11f3316e2db5dfbc578d417670de
Alexander Snitko <pennywisexy@gmail.com>
2020-09-23 17:06:15 +0300

    feat: made a function carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let k = 0.693 / HALF_LIFE_PERIOD;
   if (+sampleActivity > 0 && +sampleActivity < 15 && typeof(sampleActivity) === 'string') {
     return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / 0.693);
   } else {
