a6d226e4c5bab804e301e2875197064fd48f33d4
pennywisexy <54883206+pennywisexy@users.noreply.github.com>
2020-09-30 19:23:13 +0300

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,18 +2,21 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    let oldArr = arr;
+    let array = [];
     let count = 0;
+
+    if (arr.length === 0) {
+      return count = 1
+    }
     while (arr.length) {
       count++
       arr = arr.reduce((depth, elem) => {
         if (elem instanceof Array) depth.push(...elem);
-        
         return depth;
       }, []);
-
     }
     console.log(arr)
-    if(arr === 100000001) return count + 1
     return count
   }
 }
