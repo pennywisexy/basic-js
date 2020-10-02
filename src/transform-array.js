5e0da83b48a7c91132a4d8f414483220ff62759a
pennywisexy <54883206+pennywisexy@users.noreply.github.com>
2020-10-02 16:48:57 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,9 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(array) {
   let arr = [];
-  arr = array.filter(a => arr.push(a));
-  let oldArr = [];
-  let newArr = []
   const [discardNext, 
          discardPrev, 
          doubleNext, 
@@ -13,11 +10,10 @@ module.exports = function transform(array) {
                         '--double-next', 
                         '--double-prev'];
 
-  oldArr = arr.filter(a => a === discardNext || a === discardPrev || a === doubleNext || a === doublePrev)
-  if(oldArr.length === 0) return array;
+  arr = array.filter(a => arr.push(a));
 
   if(Array.isArray(arr)) {
-    newArr = arr.reduce((total, amount, index, arr) => {
+    arr.reduce((total, amount, index, arr) => {
       if(amount === discardNext) {
         arr[index + 1] = discardPrev;
         total.concat(arr[index + 1]);
@@ -40,5 +36,5 @@ module.exports = function transform(array) {
       return total
     }, []);
   return arr.filter(a => a !== discardNext && a !== discardPrev && a !== doubleNext && a !== doublePrev && a !== undefined);
-  }else throw new Error('Error')
+  } else throw new Error('Error')
 };
